82196ae47a2f326c2b2cec0bd19c99e0a69a33a0
hyonun321 <hyo_nun@kakao.com>
2025-04-25 10:34:08 +0900

    feat: useLocalStorage 로컬 훅 추가

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+export default function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    const json = localStorage.getItem(key);
+    return json ? JSON.parse(json) as T : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
